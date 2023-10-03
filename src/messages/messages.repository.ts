e11c87b15a8ddaa89ec9310810bb2a1cde84fee2
muazhussain <muaz.self@gmail.com>
2023-10-03 09:38:54 +0600

    Update:   - Add repository functionality.

---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,25 @@
+import { writeFile } from "fs/promises";
+import { readFile } from "fs/promises";
+
+export class MessageRepository {
+
+    async findOne(id: string) {
+        const contents = await readFile('messages.json', 'utf-8');
+        const messages = JSON.parse(contents);
+        return messages[id];
+    }
+
+    async findAll() {
+        const contents = await readFile('messages.json', 'utf-8');
+        const messages = JSON.parse(contents);
+        return messages;
+    }
+
+    async create(content: string) {
+        const contents = await readFile('messages.json', 'utf-8');
+        const messages = JSON.parse(contents);
+        const id = Math.floor(Math.random() * 999);
+        messages[id] = { id, content };
+        writeFile('messages.json', JSON.stringify(messages));
+    }
+}
